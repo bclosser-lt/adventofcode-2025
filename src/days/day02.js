6f17db961d70d5312eaa355f2ce9a1cd4e182d41
Barry Closser <1190062+bclosser-lt@users.noreply.github.com>
2025-12-11 12:40:11 -0600

    Day 2 Part 1

--- src/days/day02.js
+++ src/days/day02.js
@@ -4,8 +4,29 @@
 export const parseInput = (rawInput) => rawInput.trim().split(/\n+/);
 
 export function part1(input) {
-  // TODO: implement part 1
-  return input.length;
+  let sum = 0;
+  let invalidIDs = []
+  let ranges = input[0].split(',');
+
+  const checkID = (testRange) => {
+    const startEnd = testRange.split("-");
+    for (let id = parseInt(startEnd[0]); id <= parseInt(startEnd[1]); id++) {
+      let check = id.toString();
+
+      let pt1 = check.slice(0,check.length/2)
+      let pt2 = check.slice(check.length/2)
+
+      pt1 == pt2 ? invalidIDs.push(parseInt(check)) : null;
+
+      check.startsWith(0) ? invalidIDs.push(parseInt(id)) : null;
+    }
+  }
+
+  ranges.map((range) => checkID(range))
+  
+  invalidIDs.map((num) => sum+= num)
+  
+  return sum;
 }
 
 export function part2(input) {
