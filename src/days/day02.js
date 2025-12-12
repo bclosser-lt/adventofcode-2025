526adf104617dc0cf4805d9148812e0d843cd3bf
Barry Closser <1190062+bclosser-lt@users.noreply.github.com>
2025-12-12 13:09:12 -0600

    Day 2 refinements w/ Claude

--- src/days/day02.js
+++ src/days/day02.js
@@ -5,27 +5,32 @@ export const parseInput = (rawInput) => rawInput.trim().split(/\n+/);
 
 export function part1(input) {
   let sum = 0;
-  let invalidIDs = []
   let ranges = input[0].split(',');
 
-  const checkID = (testRange) => {
+  const checkId = (testRange) => {
     const startEnd = testRange.split("-");
     for (let id = parseInt(startEnd[0]); id <= parseInt(startEnd[1]); id++) {
       let check = id.toString();
 
-      let pt1 = check.slice(0,check.length/2)
-      let pt2 = check.slice(check.length/2)
+      // IDs that have un-even splits can be skipped
+      if (check.length % 2 === 0) {
+  
+        let pt1 = check.slice(0, check.length / 2);
+        let pt2 = check.slice(check.length / 2);
+  
+        if (pt1 === pt2) {
+          sum += id;
+          continue;
+        }
 
-      pt1 == pt2 ? invalidIDs.push(parseInt(check)) : null;
+      }
 
-      check.startsWith(0) ? invalidIDs.push(parseInt(id)) : null;
     }
+    
   }
 
-  ranges.map((range) => checkID(range))
-  
-  invalidIDs.map((num) => sum+= num)
-  
+  ranges.forEach((range) => {checkId(range)})
+
   return sum;
 }
 
